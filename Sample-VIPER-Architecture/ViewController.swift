766100684ffe0d53b2a426799df53676574b6e96
Kazuya Hatano <k28@me.com>
2024-01-16 13:10:18 +0900

    Initial Commit

---
+++ Sample-VIPER-Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample-VIPER-Architecture
+//
+//  Created by kazuya on 2024/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
